223d4994a9c54f46e2944029b87c9b94c85b547d
Yusak Setiawan <yusak.setiawan@zivlab.onmicrosoft.com>
2017-12-20 21:25:52 +0700

    First Commit

---
+++ project/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+ 
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    db.init_app(app)
+
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
---
+++ project/tests/test_users.py
@@ -0,0 +1,14 @@
+import json
+
+from project.tests.base import BaseTestCase
+
+class TestUserService(BaseTestCase):
+    """Test for the User Service."""
+
+    def test_users(self):
+        """Ensure the / ping route bahaves correctly"""
+        response = self.client.get('/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
