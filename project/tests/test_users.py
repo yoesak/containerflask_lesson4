aa24be5e010d0a8f48a0b3396adc0c428a3b8657
Yusak Setiawan <zivlab@Yusaks-MacBook-Pro.local>
2017-12-21 07:45:51 +0700

    Add User Test

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -12,3 +12,19 @@ class TestUserService(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        """Ensure a new user can be added to the database"""
+        with self.client:
+            response = self.client.post(
+                '/users/',
+                data=json.dumps(dict(
+                    username='micahel',
+                    email='michael@realpython.com'
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('michael@realpython.com was added!', data['message'])
+            self.assertIn('success', data['status'])
