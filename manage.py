08864d3debfc0c092e3a78e190ef1f31a017d5d0
Yusak Setiawan <yusak.setiawan@zivlab.onmicrosoft.com>
2017-12-22 12:35:54 +0700

    Add more function tests, and service

--- manage.py
+++ manage.py
@@ -23,5 +23,12 @@ def test():
         return 0
     return 1
 
+@manager.command
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="michael@realpython.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
+
 if __name__ == '__main__':
     manager.run()
